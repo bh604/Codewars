15595c2c7d76659c9d3fb1afdf8f9f1398b552f7
Brian Hoang <bhoangster@live.ca>
2023-02-25 17:08:09 -0800

    new codewar

--- codewars.js
+++ codewars.js
@@ -66,6 +66,19 @@
 
 // alert( isEmpty(schedule) ); // true
 
-schedule["8:30"] = "get up";
+// schedule["8:30"] = "get up";
 
-alert( isEmpty(schedule) ); // false
+// alert( isEmpty(schedule) ); // false
+
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+// For example: (Input --> Output)
+
+// 10 --> 1
+// 99 --> 18
+// -32 --> 5
+// Let's assume that all numbers in the input will be integer values.
+
+function sumDigits(number) {
+  return Math.abs(number).toString().split('').reduce(function(a,b){return +a + +b}, 0);
+}
