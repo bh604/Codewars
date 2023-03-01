b8f8f9b56e7d3f2ea0801e56b9d573e9db272d25
Brian Hoang <bhoangster@live.ca>
2023-02-28 18:29:45 -0800

    new codewar

--- codewars.js
+++ codewars.js
@@ -141,11 +141,33 @@
 // titanic.isWorthIt() // return false
 // Good luck and may you find GOOOLD!
 
-function Ship(draft,crew) {
-  this.draft = draft;
-  this.crew = crew;
-}
+// function Ship(draft,crew) {
+//   this.draft = draft;
+//   this.crew = crew;
+// }
 
-Ship.prototype.isWorthIt = function () {
-  return this.draft - (this.crew * 1.5) > 20;
+// Ship.prototype.isWorthIt = function () {
+//   return this.draft - (this.crew * 1.5) > 20;
+// }
+
+// Create new Calculator
+// importance: 5
+
+// Create a constructor function Calculator that creates objects with 3 methods:
+
+//     read() prompts for two values and saves them as object properties with names a and b respectively.
+//     sum() returns the sum of these properties.
+//     mul() returns the multiplication product of these properties.
+
+function Calculator() {
+  this.read = function() {
+    this.a = +prompt('a?', 0);
+    this.b = +prompt('b?', 0);
+  }
+  this.sum = function() {
+    return this.a + this.b;
+  }
+  this.mul = function() {
+    return this.a * this.b;
+  }
 }
