b6e2b630de85cc470b02385f08639be7ff11f95b
Brian <bhoangster@live.ca>
2023-04-14 16:23:17 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -234,6 +234,22 @@
 
 // Note: side will be an integer
 
-var cubeChecker = function(volume, side){
-  return Math.pow(side, 3) === volume && side > 0;
-};
+// var cubeChecker = function(volume, side){
+//   return Math.pow(side, 3) === volume && side > 0;
+// };
+
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+// Examples
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
