01787615f4397a6e95103c146ffd9380b5f1344e
Brian <bhoangster@live.ca>
2023-04-22 18:51:44 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -341,12 +341,18 @@
 //   return result
 // }
 
-All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
 
-Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
 
-Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 
-function feast(beast, dish) {
-	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+// function feast(beast, dish) {
+// 	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+// }
+
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(x) {
+	return typeof x === 'number' ? (x * 50) + 6 : 'Error'
 }
