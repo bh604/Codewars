909e242a68153a25e4232afe45e4ef57078fc14b
Brian Hoang <bhoangster@live.ca>
2023-04-23 21:47:24 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -351,8 +351,26 @@
 // 	return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
 // }
 
-Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
-function problem(x) {
-	return typeof x === 'number' ? (x * 50) + 6 : 'Error'
+// function problem(x) {
+// 	return typeof x === 'number' ? (x * 50) + 6 : 'Error'
+// }
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+Example: (input --> output)
+
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+
+var pairs = {'A':'T','T':'A','C':'G','G':'C'};
+
+function DNAStrand(dna){
+  return dna.split('').map(function(v){ return pairs[v] }).join('');
 }
