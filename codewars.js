72a91fe30305159e13bc330ae79b68f1a329a0e2
Brian <bhoangster@live.ca>
2023-05-06 19:10:33 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -593,3 +593,14 @@
 // "Zach" --> 18
 
 // const howManyLightsabersDoYouOwn = n => n == "Zach" ? 18 : 0;
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// Example(Input --> Output)
+
+// "john McClane" --> "McClane john"
+
+// function nameShuffler(str){
+//   //Shuffle It
+//   return str.split(' ').reverse().join(' ')
+// }
