fc542380d74afecadac3cb4b94b712dc4d9296d7
Brian <bhoangster@live.ca>
2023-05-14 22:13:20 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -672,14 +672,43 @@
 //   return fixedList
 // }
 
-Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
 
-Use conditionals to return the proper message:
+// Use conditionals to return the proper message:
 
-case	return
-name equals owner	'Hello boss'
-otherwise	'Hello guest'
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
 
-function greet (name, owner) {
-  return name === owner ? 'Hello boss' : 	'Hello guest';
-}
+// function greet (name, owner) {
+//   return name === owner ? 'Hello boss' : 	'Hello guest';
+// }
+
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+// function strCount(str, letter){  
+//   //code here
+//   let count = 0;
+//   for(let i = 0; i <= str.length-1; i++){
+//     if(str[i] === letter) {
+//       count += 1
+//     }
+//   }
+//   return count
+// }
+
+// function strCount(str, letter){  
+//   return str.split(letter).length-1
+// }
