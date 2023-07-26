b94f27fe00d77a3cbd5dff1f9ef16c8853fc0f61
Brian <bhoangster@live.ca>
2023-07-25 19:41:09 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -753,6 +753,16 @@
 // (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 // Your function should only return a number, not the explanation about how you get that number.
 
-function GetSum(a, b) {
-    return (a + b) * (Math.abs(a - b) + 1) / 2;
-  }
+// function GetSum(a, b) {
+//     return (a + b) * (Math.abs(a - b) + 1) / 2;
+//   }
+
+We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+All values will be positive integers or floats, or zero.
+
+function plural(n) {
+  return n === 1 ? false : true;
+}
