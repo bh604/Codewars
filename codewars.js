27f84bf3eb9a14f7390462672324c259e1931e54
Brian <bhoangster@live.ca>
2024-03-12 17:45:10 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -757,12 +757,23 @@
 //     return (a + b) * (Math.abs(a - b) + 1) / 2;
 //   }
 
-We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
 
-You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
 
-All values will be positive integers or floats, or zero.
+// All values will be positive integers or floats, or zero.
 
-function plural(n) {
-  return n === 1 ? false : true;
+// function plural(n) {
+//   return n === 1 ? false : true;
+// }
+
+Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+
+function solution(nums){
+  return nums !== null ? nums.sort(function(a,b){return a-b}) : [];
 }
