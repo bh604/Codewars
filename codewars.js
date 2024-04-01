f5378f62c6dfc4c03740ca4224598198035a9262
Brian <bhoangster@live.ca>
2024-03-31 18:07:34 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -941,3 +941,18 @@
 // }
 
 // let boolToWord = (bool) => (bool === true ? `Yes` : `No`);
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// function numberToString(num){
+//   return num.toString()
+// }
+
+// let numberToString = num => num.toString()
