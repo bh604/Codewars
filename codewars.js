4527d5b90c9706366cc8a50ff048a641ddeae11d
Brian <bhoangster@live.ca>
2024-04-07 12:20:29 -0700

    new codewar

--- codewars.js
+++ codewars.js
@@ -964,3 +964,11 @@
 // 5, "Hello" -> "HelloHelloHelloHelloHello"
 
 // let repeatStr = (n, s) => s.repeat(n);
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// let repeatStr = (n,s) => s.repeat(n);
